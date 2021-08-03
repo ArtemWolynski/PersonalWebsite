f11313d365334661c17a669b1cc1e18c594b191d
ArtemVolynskiy <art.volynskiy@gmail.com>
2021-08-03 16:50:29 +0700

    Forgotten nav effect uploaded

---
+++ src/app/store/effects/navigation.effects.ts
@@ -0,0 +1,31 @@
+import {Injectable} from '@angular/core';
+import {Actions, createEffect, ofType} from '@ngrx/effects';
+import {map} from 'rxjs/operators';
+import {Router} from '@angular/router';
+import {navScrollToElement, navSlideToElement} from '../actions/navigation.actions';
+import {ScreenTransitionService} from '../../services/screen-transition.service';
+
+@Injectable()
+export class NavigationEffects {
+
+  constructor(
+    private _router: Router,
+    private actions$: Actions,
+    private screenTransitionService: ScreenTransitionService
+  ) {}
+
+  changeUrl = createEffect(() => this.actions$.pipe(
+    ofType(navSlideToElement),
+    map(action => {
+      this._router.navigate([action.currentScreen.toLocaleLowerCase()]).then();
+      }
+    )
+  ), { dispatch: false});
+
+  scrollToElement = createEffect( () => this.actions$.pipe(
+    ofType(navScrollToElement),
+    map(action => {
+      this.screenTransitionService.scrollToElement(action.currentScreen);
+    })
+  ), { dispatch: false});
+}
