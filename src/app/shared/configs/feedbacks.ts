1cd8b720387f7573fdfdae559c77b45aaf346e4f
ArtemVolynskiy <art.volynskiy@gmail.com>
2021-08-10 15:24:25 +0700

    Real recommendations added

--- src/app/shared/configs/feedbacks.ts
+++ src/app/shared/configs/feedbacks.ts
@@ -1,18 +1,30 @@
 export const feedbacks = [
   {
-    name: 'Ardrey Rebrov',
-    position: 'Chief Technical Officer at Scentbird',
-    review: 'Artem is amazing, be like Artem. Artem is amazing, be like Artem.Artem is amazing, be like Artem.Artem is amazing, be like Artem.Artem is amazing, be like Artem.Artem is amazing, be like Artem.\n' +
-    '          Artem is amazing, be like Artem.Artem is amazing, be like Artem.Artem is amazing, be like Artem.\n' +
-    '          Artem is amazing, be like Artem.Artem is amazing, be like Artem.Artem is amazing, be like Artem.\n' +
-    '          Artem is amazing, be like Artem.Artem is amazing, be like Artem.\n' +
-    '          Artem is amazing, be like Artem.',
-    imgUrl: '../../../../assets/images/Artem%20Wolynski.png'
+    name: 'Ardrei Rebrov',
+    position: 'CTO & Co-founder at Scentfird & Deck of Scarlet',
+    review: 'It was great to work with Artem. ' +
+      'He was eager to learn new things both in engineering and people management, ' +
+      'he delivered what he promised and was always ready to take into new responsibility. ' +
+      'He is a guy who is ready to jump into fast moving company if he sees a great team and a great product.',
+    imgUrl: '../../../../assets/images/andrei_rebrov.jpg'
   },
   {
-    name: 'Ardrei Maximov',
+    name: 'Ardrew Maximov',
     position: 'Chief Executive Officer at Precious Payload',
-    review: 'Excellent soft skillSets, great team player, perfect fit for a remote team, self-assessment and self-reflection, always learning new technologies. I would recommend Artem for any startup as a lead developer and a great addition to the product team. Artem used to be employee #2 at Precious Payload and I cannot recommend him more!',
-    imgUrl: '../../../../assets/images/Artem%20Wolynski.png'
+    review: 'Excellent soft skillSets, great team player, perfect fit for a remote team, ' +
+      'self-assessment and self-reflection, always learning new technologies. ' +
+      'I would recommend Artem for any startup as a lead developer and a great addition to the product team. ' +
+      'Artem used to be employee #2 at Precious Payload and I cannot recommend him more!',
+    imgUrl: '../../../../assets/images/andrew_max.jpeg'
+  },
+  {
+    name: 'Sergei Kharlamov',
+    position: 'Full Stack Web Developer & Software Engineer at Precious Payload',
+    review: 'It was very comfortable for me to work with Artem on the same team.' +
+      ' He has high professional skills as a lead developer. He is very attentive to business requirements, ' +
+      'knows how to plan work well, thereby increasing the efficiency of development. ' +
+      'I learned a lot from Artem and am grateful for this experience.\n' +
+      'I recommend Artem as a full-stack developer, team lead, or project manager.',
+    imgUrl: '../../../../assets/images/sergei_kharlamov.jpg'
   }
 ];
