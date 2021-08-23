983431be329bc8e25b652c67abfb391d507ae51f
ArtemWolynski <art.volynskiy@gmail.com>
2021-08-23 15:52:52 +0700

    AR name fix (sry Andrei)

--- src/app/shared/configs/feedbacks.ts
+++ src/app/shared/configs/feedbacks.ts
@@ -1,35 +1,38 @@
-import {Recommendation} from '../../core/models/recommendation';
+import { Recommendation } from '../../core/models/recommendation';
 
 export const recommendations: Recommendation[] = [
   {
     id: 0,
-    author: 'Ardrei Rebrov',
+    author: 'Andrei Rebrov',
     position: 'CTO & Co-founder at Scentfird & Deck of Scarlet',
-    text: 'It was great to work with Artem. ' +
+    text:
+      'It was great to work with Artem. ' +
       'He was eager to learn new things both in engineering and people management, ' +
       'he delivered what he promised and was always ready to take into new responsibility. ' +
       'He is a guy who is ready to jump into fast moving company if he sees a great team and a great product.',
-    imgUrl: '../../../../assets/images/andrei_rebrov.jpg'
+    imgUrl: '../../../../assets/images/andrei_rebrov.jpg',
   },
   {
     id: 1,
     author: 'Ardrew Maximov',
     position: 'Chief Executive Officer at Precious Payload',
-    text: 'Excellent soft skills, great team player, perfect fit for a remote team, ' +
+    text:
+      'Excellent soft skills, great team player, perfect fit for a remote team, ' +
       'self-assessment and self-reflection, always learning new technologies. ' +
       'I would recommend Artem for any startup as a lead developer and a great addition to the product team. ' +
       'Artem used to be employee #2 at Precious Payload and I cannot recommend him more!',
-    imgUrl: '../../../../assets/images/andrew_max.jpeg'
+    imgUrl: '../../../../assets/images/andrew_max.jpeg',
   },
   {
     id: 2,
     author: 'Sergei Kharlamov',
     position: 'Full Stack Web Developer & Software Engineer at Precious Payload',
-    text: 'It was very comfortable for me to work with Artem on the same team.' +
+    text:
+      'It was very comfortable for me to work with Artem on the same team.' +
       ' He has high professional skills as a lead developer. He is very attentive to business requirements, ' +
       'knows how to plan work well, thereby increasing the efficiency of development. ' +
       'I learned a lot from Artem and am grateful for this experience.\n' +
       'I recommend Artem as a full-stack developer, team lead, or project manager.',
-    imgUrl: '../../../../assets/images/sergei_kharlamov.jpg'
-  }
+    imgUrl: '../../../../assets/images/sergei_kharlamov.jpg',
+  },
 ];
--- src/app/shared/configs/portfolio.ts
+++ src/app/shared/configs/portfolio.ts
@@ -1,41 +1,45 @@
-import {Project} from '../../core/models/project';
+import { Project } from '../../core/models/project';
 
 export const portfolio: Project[] = [
   {
     id: 0,
     name: 'thepuir',
-    description: "This project is here because it looks dope! And yeah, I'm the one who created it. " +
-    "Another thing that makes it special is that it was not created as a freelance job (I've never done freelance, at least now the classic version of it). " +
-    "It was a (quite unusual) present for a very good friend of mine, trying to start a new business. Some things are worth doing for free, you know. " +
-    "Once again, as a part of the project I've worked with designers to create the concept of the landing page. Set up the customer care pipeline. " +
-    "Taught people how to track conversion rates. Set up the adSense and CRM systems to attract and track leads. " +
-    "To sum it up, all you need to make your business digital is a nerdy friend willing to do something good. Or cash.",
+    description:
+      "This project is here because it looks dope! And yeah, I'm the one who created it. " +
+      "Another thing that makes it special is that it was not created as a freelance job (I've never done freelance, at least now the classic version of it). " +
+      'It was a (quite unusual) present for a very good friend of mine, trying to start a new business. Some things are worth doing for free, you know. ' +
+      "Once again, as a part of the project I've worked with designers to create the concept of the landing page. Set up the customer care pipeline. " +
+      'Taught people how to track conversion rates. Set up the adSense and CRM systems to attract and track leads. ' +
+      'To sum it up, all you need to make your business digital is a nerdy friend willing to do something good. Or cash.',
     link: 'https://thepuir.com/',
     imgUrl: '../../assets/images/tp.png',
   },
   {
     id: 1,
     name: 'creative digital',
-    description: "Imagine yourself recently graduated from the university. " +
-    "Living abroad and having zero experience in programming field. What would any responsible guy do? That's right! " +
-    "Open a web studio and gain real experience. Creative digital is a brain child of mine. Made by me from the very scratch. " +
-    "From idea to final design (with moderate amount of help form professional designer). I've even created a logo myself. " +
-    "It's placed here because it means so much to me. It's my first experience with front-end. " +
-    "First experience with angular and a thing that helped me to make first money from programming and gain first experience. " +
-    "I'm deliberately keeping it as it was when I wrote it, to remind myself about those days. " +
-    "Go visit the website and check out how I was able to create an adaptive layout without even knowing what the hell it is.",
+    description:
+      'Imagine yourself recently graduated from the university. ' +
+      "Living abroad and having zero experience in programming field. What would any responsible guy do? That's right! " +
+      'Open a web studio and gain real experience. Creative digital is a brain child of mine. Made by me from the very scratch. ' +
+      "From idea to final design (with moderate amount of help form professional designer). I've even created a logo myself. " +
+      "It's placed here because it means so much to me. It's my first experience with front-end. " +
+      'First experience with angular and a thing that helped me to make first money from programming and gain first experience. ' +
+      "I'm deliberately keeping it as it was when I wrote it, to remind myself about those days. " +
+      'Go visit the website and check out how I was able to create an adaptive layout without even knowing what the hell it is.',
     link: 'https://creativedigitalvn.com/',
     imgUrl: '../../assets/images/cd.png',
   },
   {
     id: 2,
     name: 'precious payload',
-    description: "Precious payload it here for many reasons. First of all it is a place where I met lots of amazing people. " +
-    "Place where I was able to learn what the real responsibility is. Place where I was able to create multiple projects from scratch. " +
-    "Precious Payload is a place where you can grow like crazy and feel that your work means something. I've spent to years there. " +
-    "Was responsible for all the software company generated, literally." +
-    " And yeah, we've helped to send those tiny metal boxes called smallsats (fancy word for small satellites) to space, how cool is that!",
+    description:
+      'Precious payload it here for many reasons. First of all it is a place where I met lots of amazing people. ' +
+      'Place where I was able to learn what the real responsibility is. Place where I was able to create multiple projects from scratch. ' +
+      "Precious Payload is a place where you can grow like crazy and feel that your work means something. I've spent four years there. " +
+      'Was responsible for all the software company generated, literally.' +
+      " And yeah, we've helped to send those tiny metal boxes called smallsats (fancy word for small satellites) " +
+      'to space, how cool is that!',
     link: 'https://preciouspayload.com',
     imgUrl: '../../assets/images/pp.png',
-  }
+  },
 ];
